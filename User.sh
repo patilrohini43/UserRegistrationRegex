67d7a9f831c5f4b12c550a3d8cf354b2cb703a80
rohini <patilrohini43@gmail.com>
2020-01-11 11:42:28 +0530

    Added:first name regex pattern

--- User.sh
+++ User.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "User Registration"
+
+read -p "Enter User First Name" firstName
+pat=^[A-Z]{1}[a-z]{2}[a-z]*$
+if [[ $firstName =~ $pat ]]
+then
+	echo "Good Name"
+else
+	echo "Invalid Name Please Enter First Char only Upper Case"
+fi
+
+
