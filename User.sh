14ef3d5641b434615c24a1156b4aeef9dbf191fd
rohini <patilrohini43@gmail.com>
2020-01-11 11:46:33 +0530

    Added:accpet last name from user and cheking pattern

--- User.sh
+++ User.sh
@@ -3,8 +3,11 @@
 echo "User Registration"
 
 read -p "Enter User First Name" firstName
+read -p "Enter User Last Name" lastName
+
 pat=^[A-Z]{1}[a-z]{2}[a-z]*$
-if [[ $firstName =~ $pat ]]
+
+if [[ $firstName =~ $pat ]] && [[ $lastName =~ $pat ]]
 then
 	echo "Good Name"
 else
